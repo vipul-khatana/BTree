350f31efd708adc02b183b912f7dbb25076cc204
Vipul Khatana <vipulkhatana96@gmail.com>
2017-12-03 22:29:45 +0530

    Update IllegalKeyException.java

--- IllegalKeyException.java
+++ IllegalKeyException.java
@@ -1,5 +1,8 @@
-// package col106.a3;
+/*
+Created Sep Jun 10 2015
 
+@author: vipulkhatana
+*/
 public class IllegalKeyException extends Exception {
     public IllegalKeyException() {
         super("IllegalKeyException");
